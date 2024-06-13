ccf5a16ece807bc4fcaa472ef6277745a04ba255
moonhr <moonhr153@gmail.com>
2024-06-13 21:13:00 +0900

    key export

---
+++ restApi-practice/dist/key.js
@@ -0,0 +1,4 @@
+const endpoint = `http://apis.data.go.kr/1400000/forestStusService`;
+const keyName = `SP6poFNj53tVrUmPFA7YiAuohTBnOXQ%2BUE4J6uq0ItPV49i%2Bu2%2FVXlNUnq5v3WumEyZthjVOj9wG%2FotkQW%2F5kg%3D%3D`;
+export const url = `https://apis.data.go.kr/1400000/forestStusService/getfirestatsservice?serviceKey=${keyName}&numOfRows=10&pageNo=1&searchStDt=20160101&searchEdDt=20160825`;
+// exports.module(url)
---
+++ restApi-practice/src/consroller.ts
@@ -0,0 +1,19 @@
+import { url } from "./key";
+
+const AJAX = () => {
+  const xhr = new XMLHttpRequest()
+  xhr.open('GET', `${url}`, true)
+
+  xhr.addEventListener('load', () => {
+    if (xhr.status === 200) {
+      console.log("데이터 받은 후 조회")
+
+      const result = JSON.parse(xhr.responseText)
+      console.timeLog(result)
+      
+
+    }
+  })
+  xhr.send();
+}
+AJAX();
